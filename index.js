b52280dff9da5f2614b633e2b0098dbe3de60c99
Dmitry Kvasov <kvasov85@mail.ru>
2018-10-31 19:33:28 +0300

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return <div />;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
