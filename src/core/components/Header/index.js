9b047e095746dfd11ffeac5c97a3b5001625a657
Dmitry Kvasov <kvasov85@mail.ru>
2018-11-15 12:39:22 +0300

    [SH-03] header stateless

--- src/core/components/Header/index.js
+++ src/core/components/Header/index.js
@@ -5,18 +5,15 @@ import HeaderCart from '../HeaderCart/index';
 
 import './style.scss';
 
-class Header extends React.PureComponent {
-  state = {};
-  render() {
-    return (
-      <header className="header">
-        <div className={classNames('header__container', 'container')}>
-          <a href="#" className="header__logo" />
-          <HeaderCart />
-        </div>
-      </header>
-    );
-  }
+function Header() {
+  return (
+    <header className="header">
+      <div className={classNames('header__container', 'container')}>
+        <a href="#" className="header__logo" />
+        <HeaderCart />
+      </div>
+    </header>
+  );
 }
 
 export default Header;
