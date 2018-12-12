b54fca4946d24753bb05a5c4f124c2234bec7951
Dmitry Kvasov <kvasov85@mail.ru>
2018-12-12 11:59:39 +0300

    [SH-07] middleware API

--- src/reducers/Cart.js
+++ src/reducers/Cart.js
@@ -10,14 +10,12 @@ const initialState = {
 export default function(state = initialState, action) {
   switch (action.type) {
     case types.ADD_PRODUCT:
-      // Так вообще можно делать? объявлять переменные в case.
-      // eslint не одобряет :)
       const products = state.products.concat([action.product]);
       return assign({}, initialState, { products });
     case types.REMOVE_PRODUCT:
       const res = [];
-      map(state.products, (product, i) => {
-        if (action.product !== i) {
+      map(state.products, product => {
+        if (action.product !== product.id) {
           res.push(product);
         }
       });
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 
-import thunk from 'redux-thunk';
+import APIMiddleWare from '../middleware/API';
 import DevTools from '../containers/DevTools';
 
 import reducers from '../reducers/index';
@@ -8,7 +8,7 @@ import reducers from '../reducers/index';
 const store = createStore(
   reducers,
   compose(
-    applyMiddleware(thunk),
+    applyMiddleware(APIMiddleWare),
     DevTools.instrument()
   )
 );
